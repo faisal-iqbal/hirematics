96de21d99a0b1baa86fd9ba31b0354621f1c8299
Sameer Ahmed Siddiqui <sameerdgrea8@yahoo.com>
2013-08-07 15:04:46 +0500

    Fixing the condition of current_user.present

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,5 +1,5 @@
 class Source < ActiveRecord::Base
-   if current_user.company_id?
+   if current_user.present? and current_user.company_id.present?
     default_scope where(:company_id => current_user.company_id)
   else
     default_scope where(:company_id => $current_company_id)
